0c317037c687c78077c099867db9be9ec0e2532d
Julius <gwolojulius2@gmail.com>
2024-09-25 05:59:10 +0200

    Left column of the About Section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
 function App() {
   return (
     <>
       <Navbar />
-    
+    <div className="container main">
+      <HeroSection/>
+    </div>
     </>
   );
 }
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="section">
+      <div className="col">
+        <h1 className="title">What Are We About</h1>
+        <p className="info">FoodHub is a place where you can please your soul and tummy with delicious food recepies of all cuisine. And our service is absolutely free. So start exploring now</p>
+        <button className="btn">explore now</button>
+      </div>
+      <div className="col">col 2</div>
+    </div>
+  );
+};
+
+export default HeroSection;
