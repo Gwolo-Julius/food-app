e2dd5b1f0e9ad2714739608bc4ab29f5fcf68607
Julius <gwolojulius2@gmail.com>
2024-09-27 13:03:50 +0200

    Initating Recipes page

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Recipes = () => {
+  const searches = [
+    "Cookies",
+    "ice cream",
+    "burger",
+    "lasagna",
+    "salad",
+    "juice",
+    "biriyani",
+    "Pizza",
+    "pudding",
+    "soup",
+  ];
   return (
-    <div className='recipes'>
+    <div className="previour-search">
+      <h2>Previours Searches</h2>
+      <div className="previour-search-container">{searches.map(search=>(<div>{search}</div>))}</div>
       <h1>I'm Recipes page</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Recipes
+export default Recipes;
